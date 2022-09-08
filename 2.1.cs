a2e01bddd9d3fb03d59d0d074f68ee7f4f5f5f3a
DenisBilokon <106659593+DenisBilokon@users.noreply.github.com>
2022-09-08 19:39:59 +0300

    4 task here
    hurray i did it

---
+++ 2.1.cs
@@ -0,0 +1,112 @@
+﻿namespace Hw2._1
+{
+    class Adress
+    {
+        string index;
+        public string Index
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return index;
+            }
+        }
+        private string country;
+        public string Country
+        {
+            set
+            {
+                country = value;
+            }
+            get
+            {
+                return country;
+            }
+        }
+        string city;
+        public string City
+        {
+            set
+            {
+                city = value;
+            }
+            get
+            {
+                return city;
+            }
+        }
+        string street;
+        public string Street
+        {
+            set
+            {
+                street = value;
+            }
+            get
+            {
+                return street;
+            }
+        }
+        string house;
+        public string House
+        {
+            set
+            {
+                house = value;
+            }
+            get
+            {
+                return house;
+            }
+        }
+        string apartment;
+        public string Apartment
+        {
+            set
+            {
+                apartment = value;
+            }
+            get
+            {
+                return apartment;
+            }
+        }
+        string ggg;
+        public string GGG
+        {
+            get
+            {
+                return new string('-', 50);
+            }
+        }
+        class Program
+        {
+            static void Main()
+            {
+                Adress program = new Adress();
+                program.Index = "04205";
+                Console.WriteLine(program.Index);
+                Console.WriteLine(new string('-', 50));
+                program.Country = "Ukraine";
+                Console.WriteLine(program.Country);
+                Console.WriteLine(new string('-', 50));
+                program.City = "Rivne";
+                Console.WriteLine(program.City);
+                program.City = "Kyiv";
+                Console.WriteLine(program.City);
+                program.City = "Lviv";
+                Console.WriteLine(program.City);
+                Console.WriteLine(program.GGG);
+                Console.WriteLine(program.GGG);
+                Console.WriteLine(program.GGG);
+                Console.WriteLine(program.GGG);
+                Console.WriteLine(program.GGG);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ 2.4.cs
@@ -0,0 +1,26 @@
+﻿
+class User
+{
+    public int age = 18;
+    public string login = "Rollen", name = "Denis", surname = "Bilokon";
+    private string date = "08.09.2022"; 
+
+    public void Print ()
+    {
+        Console.WriteLine("age: " + age);
+        Console.WriteLine("login: " + login);
+        Console.WriteLine("name: " + name);   
+        Console.WriteLine("surname: " + surname); 
+        Console.WriteLine("date: " + date);   
+    }
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        User Den = new User ();
+        Den.Print ();
+
+    }
+
+}
